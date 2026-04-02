8ce80de99d61f500b27ee11bb294a28007d24e40
kannireddyVLMP <kannireddy614@gmail.com>
2026-04-02 21:12:48 +0530

    orderpage change

--- src/test/java/com/Pearson/Pages/Order.java
+++ src/test/java/com/Pearson/Pages/Order.java
@@ -55,6 +55,7 @@ public class Order extends Base {
             logger.info("Validating product name on order page...");
             ExtentLogger.info("Validating product name on order page...");
             System.out.println("Validating product name on order page...");
+            System.out.println("Expected product: " + expectedProduct);
             // ✅ Wait for all product elements to be visible
             List<WebElement> productElements = cm.waitForElements(By.xpath("//td[@class='line-item product-info-column m-3']/div[@class='title']"), 10);
             WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
